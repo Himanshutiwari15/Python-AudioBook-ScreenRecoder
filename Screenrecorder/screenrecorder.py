5e16fea426683fb117b76f025f75989d26280e6c
Yamini-8750 <60479140+Yamini-8750@users.noreply.github.com>
2020-10-02 13:16:00 +0530

    Create screenrecorder.py

---
+++ Screenrecorder/screenrecorder.py
@@ -0,0 +1,15 @@
+import numpy as np
+from PIL import ImageGrab
+import cv2
+fourcc = cv2.VideoWriter_fourcc(*"XVID")
+size = (ImageGrab.grab()).size
+output = cv2.VideoWriter("output.mp4",fourcc,5.0,size)
+while True:
+    img = np.array(ImageGrab.grab())
+    frame = cv2.cvColor(img,cv2.COLOR_BGR2RGB)
+    cv2.imshow("Screen",frame)
+    output.write(img)
+    if cv2.waitkey(1)==27:
+       break
+output.release()
+cv2.destroyAllWindows
